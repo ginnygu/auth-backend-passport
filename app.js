da64d63aa783c47bbee854fd1b246113ed354474
spaceforestchu <spaceforestchu@gmail.com>
2021-08-12 10:26:36 -0400

    second

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var usersRouter = require("./routes/users/usersRouter");
 
 var app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(logger("dev"));
 app.use(express.json());
